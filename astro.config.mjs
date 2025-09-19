e4fb8ce6e785f39452282d828f84711d9c18ac35
hftos <marcos.mvegas@gmail.com>
2025-09-18 22:28:33 -0400

    (14)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jitwebservices.github.io',
   base: '/themoney-printingpress',
   output: 'static',
+  trailingSlash: 'ignore',
+  build: {
+    format: 'directory'
+  },
   vite: {
     plugins: [tailwindcss()],
   },
